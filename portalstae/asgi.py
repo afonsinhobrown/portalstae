d882ea30e59a014fb69bc1971af21aeb5218b552
afonsinhobrown <afonsinhobrown@github.com>
2026-03-19 11:10:42 +0200

    Corrigir AppRegistryNotReady: Ordem correcta de inicializacao no ASGI

--- portalstae/asgi.py
+++ portalstae/asgi.py
@@ -3,15 +3,21 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 
+# 1. Definir o ambiente
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portalstae.settings')
-# django.setup() foi removido pois o get_asgi_application() ja o faz implicitamente.
 
+# 2. Inicializar o Django e obter a aplicação HTTP primeiro
+# Isso garante que o AppRegistry esteja pronto antes de importarmos o routing/consumers
+django_asgi_app = get_asgi_application()
+
+# 3. Agora podemos importar o resto com segurança
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import recursoshumanos.routing
 
+# 4. Definir o roteador final
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             recursoshumanos.routing.websocket_urlpatterns
